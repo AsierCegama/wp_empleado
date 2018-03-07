7edd4d0d893a41141aa6655d8312c995241b4f14
asier <garviel1988@gmail.com>
2018-03-07 22:15:20 +0100

    primer commit

---
+++ vistas/form_Mostrar.php
@@ -0,0 +1,28 @@
+<?php
+
+	foreach ($empleados as $empleado) {
+		echo "<label id='nombre' >Nombre:</label>";
+		echo $empleado->nombre;
+		echo "<br /><br />";
+		echo "<label id='apellido' >Apellido:</label>";
+		echo $empleado->apellido;
+		echo "<br/><br/>";
+		echo "<label id='nss' >Numero de la seguridad social:</label>";
+		echo $empleado->nss;
+		echo "<br/><br/>";
+		echo "<label id='fijo' >Fijo:</label>";
+		echo $empleado->fijo;
+		echo "<br/><br/>";
+		echo "<label id='ventas' >Ventas brutas:</label>";
+		echo $empleado->ventas;
+		echo "<br/><br/>";
+		echo "<label id='tarifa' >Tarifa comisión:</label>";
+		echo $empleado->tarifa;
+		echo "<br/><br/>";
+		echo "<hr/>";
+	}
+
+?>
+
+
+
---
+++ wp_empleado.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+ *  	
+  Plugin Name: Empleados
+  Description: Plugin de wordpres que permite insertar y mostrar empleados. Trabajo de clase
+  Version: 1.0
+  Author: Asier Cegama
+ * 
+ */
+require_once (plugin_dir_path(__FILE__) . 'config/config.php');
+include_once(plugin_dir_path(__FILE__) . 'modelo/DataBase.php');
+
+register_activation_hook(__FILE__, 'crearTabla');
+register_deactivation_hook(__FILE__, 'borrarTabla');
+
+function crearTabla() {
+    global $wpdb;
+    $nomTabla = $wpdb->prefix.TABLA;
+    require_once( ABSPATH.'wp-admin/includes/upgrade.php' );
+    $creado = dbDelta('create table ' . $nomTabla . ' (nombre varchar(50), apellido varchar(50), nss varchar(50), fijo int(50), ventas float, tarifa float);');
+}
+
+function borrarTabla() {
+    global $wpdb;
+    $nomTabla = $wpdb->prefix . TABLA;
+
+    $sql = "drop table if exists " . $nomTabla . ";";
+    $wpdb->query($sql);
+}
+
+function insertar() {
+    include(plugin_dir_path(__FILE__) . 'vistas/form_Insertar.php');
+}
+
+function listar() {
+    $empleados = DataBase::mostrarEmpleado();
+    include(plugin_dir_path(__FILE__) . 'vistas/form_Mostrar.php');
+}
+
+add_shortcode('Insertar_Empleado', 'insertar');
+add_shortcode('Listar_Empleados', 'listar');
